d187646f4368ff62c35ac4f55812138c65f0ccd9
ashinon <nanase.a.1071607@gmail.com>
2020-04-19 21:38:25 +0900

    [modify] イベントリスナーのonceオプションを追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import Portfolio from './portfolio.js';
-document.addEventListener('DOMContentLoaded', () => {
-  new Portfolio();
-});
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    new Portfolio();
+  },
+  { once: true }
+);
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = Array.prototype.forEach;
 }
